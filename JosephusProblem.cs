b3183a1c460e039d1063ae40b440f95ef8622144
CaptLJS <47562344+CaptLJS@users.noreply.github.com>
2019-03-12 03:27:37 +0530

    Add files via upload

---
+++ JosephusProblem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Josephus
+{
+
+    static int josephus(int n, int k)
+    {
+        if (n == 1)
+            return 1;
+        else
+            return (josephus(n - 1, k) + k - 1) % n + 1;
+    }
+
+
+    public static void Main()
+    {
+        int n, step;
+        Console.WriteLine("Enter number of people");
+        n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter step");
+        step = int.Parse(Console.ReadLine());
+        Console.WriteLine("Alive at position " + josephus(n, step));
+    }
+}
+
